68d4224bbd1281c7fc51afc9bf0cd861ce11c457
Francisco <franmira2003@gmail.com>
2025-11-08 21:05:53 +0100

    yolo detector with head detection

--- perception/alars/auv_yolo_detector/launch/yolo_detector_launch.py
+++ perception/alars/auv_yolo_detector/launch/yolo_detector_launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument,LogInfo
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -7,13 +7,17 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
 
     # ---- frequently changed params as launch arguments ...
-    mode_arg = DeclareLaunchArgument('mode', default_value='sim')
-    inference_frequency_arg = DeclareLaunchArgument('inference_frequency', default_value='0.5')
+    mode_arg = DeclareLaunchArgument('mode', default_value='real')
+    namespace_arg = DeclareLaunchArgument('namespace', default_value='Quadrotor')
+    device_arg = DeclareLaunchArgument('namespace', default_value='cpu')
+    inference_frequency_arg = DeclareLaunchArgument('inference_frequency', default_value='0.1')
     model_path_arg = DeclareLaunchArgument('model_path',
-                                       default_value = '/home/fm/KTH_Courses/ResearchProject/RProj_GitRepoFork/colcon_ws/src/smarc2/perception/auv_yolo_detector')
+                                       default_value = '/home/fm/KTH_Courses/ResearchProject/RProj_GitRepoFork/colcon_ws/src/smarc2/perception/alars/auv_yolo_detector')
 
     # ... and as node params
     mode = LaunchConfiguration('mode')
+    namespace = LaunchConfiguration('namespace')
+    device = LaunchConfiguration('device')
     inference_frequency = LaunchConfiguration('inference_frequency')
     model_path = LaunchConfiguration('model_path')
 
@@ -27,21 +31,29 @@ def generate_launch_description():
     detector_node = Node(
         package='auv_yolo_detector',
         executable='auv_yolo_detector',
-        namespace='Quadrotor',
+        namespace=namespace,
         output='screen',
         parameters=[
             config_file,
             {
                 'mode': mode,
+                'namespace': namespace,
+                'device': device,
                 'inference_frequency': inference_frequency,
-                'model_path': model_path
+                'model_path': model_path,
+                
             }
         ]
     )
 
     return LaunchDescription([
+        namespace_arg,
         mode_arg,
+        device_arg,
         inference_frequency_arg,
+        LogInfo(msg=["[Launch] mode argument = ", mode]),
+        LogInfo(msg=["[Launch] namespace argument = ", namespace]),
+        LogInfo(msg=["[Launch] device argument = ", device]),
         model_path_arg,
         detector_node
     ])
