659e9647953c19de5040bb4fe73e2519b1b5ea2e
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-11-10 13:55:16 +0100

    Added udev rules for pocket3

--- scripts/smarc_bringups/scripts/dji_bringup.sh
+++ scripts/smarc_bringups/scripts/dji_bringup.sh
@@ -184,7 +184,7 @@ if [[ $USE_SIM_TIME = "False" ]]; then
     # for basic usb webcam
     #tmux send-keys "ros2 run usb_cam usb_cam_node_exe --ros-args -r __ns:=/$ROBOT_NAME/gimbal_camera" C-m
     # for the dji gimbal cam
-    GSCAM_CONFIG="v4l2src device=/dev/video0 ! image/jpeg,width=1920,height=1080,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw,format=BGR"
+    GSCAM_CONFIG="v4l2src device=/dev/djipocket3 ! image/jpeg,width=1920,height=1080,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw,format=BGR"
     tmux send-keys "ros2 run gscam gscam_node --ros-args \
     -p gscam_config:=\"$GSCAM_CONFIG\" \
     -p frame_id:=osmo3_optical_frame \
